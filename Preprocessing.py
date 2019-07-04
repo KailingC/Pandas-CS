46931d85bbfadb53e11652f19b81af6c9dfc8e65
KailingC <45678474+KailingC@users.noreply.github.com>
2019-07-04 08:55:47 +0800

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,3 +3,7 @@
 df.rename(columns={'oldName1': 'newName1', 'oldName2': 'newName2'}, inplace=True)
 #rename all the columns
 df.columns = ['a', 'b']
+
+
+#2 filter based on a list
+df = df[df['subject_1'].isin([1, 2, 13, 18, 25])]
